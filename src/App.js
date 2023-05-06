2e6eaab8a139de401801ed64eb8ae62a127c3261
ijnam7 <ijnam7@gmail.com>
2023-05-07 03:11:24 +0900

    study 230507

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = "맛있는 육회 비빔밥"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div style={ {color: "blue", fontSize: "30px"} }>개발자 Blog</div>
+      </div>
+      <h4> { posts } </h4>
     </div>
   );
 }
